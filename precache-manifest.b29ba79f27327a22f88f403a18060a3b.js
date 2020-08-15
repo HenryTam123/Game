0bad03cda8c0b9050cc50440663594e5d8558edf
HenryTam123 <u3557967@connect.hku.hk>
2020-08-15 15:17:08 +0800

    Updates

---
+++ precache-manifest.b29ba79f27327a22f88f403a18060a3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd8e0a77f8ecd153e197a6e61395d45",
+    "url": "/Game/index.html"
+  },
+  {
+    "revision": "0f101c6adc0ce5e67a9a",
+    "url": "/Game/static/css/main.f10d1ea1.chunk.css"
+  },
+  {
+    "revision": "583275a7f6f78c1a3966",
+    "url": "/Game/static/js/2.5755b8e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Game/static/js/2.5755b8e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f101c6adc0ce5e67a9a",
+    "url": "/Game/static/js/main.c65ae459.chunk.js"
+  },
+  {
+    "revision": "78583a25cd4987f43431",
+    "url": "/Game/static/js/runtime-main.fbd8eaf8.js"
+  }
+]);
